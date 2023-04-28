35e2a0445e5f218f6fad2246d930c43f4b9d684f
gxnas <52808122+gxnas@users.noreply.github.com>
2023-04-28 09:22:01 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,6 +4,13 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
+# 添加分区扩容脚本
+git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+
+# 添加网速测试脚本
+git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+
+
 # 后台IP设置
 export Ipv4_ipaddr="192.168.1.11"           # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
